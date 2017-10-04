663739182253c3272fae8ca34f37bae109f42c42
Kevin Herr <kherr9@gmail.com>
2017-10-04 17:49:41 -0500

    Feature/store delete get (#2)
    * add get, store, and delete
    
    * install dotnet sdk
    
    * try different script
    
    * remove
    
    * start emulator
    
    * space
    
    * remove xunit runner
    
    * refactor
    
    * fix
    
    * fix
    
    * touch
    
    * Revert "touch"
    
    This reverts commit 4d850d2cabcfe3155cae908a21a4b904eab86008.

---
+++ src/ToyStorage.UnitTests/CloudStorageAccountHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ToyStorage.UnitTests
+{
+    public static class CloudStorageAccountHelper
+    {
+        public static CloudStorageAccount CreateCloudStorageAccount()
+        {
+            return CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+        }
+
+        public static CloudBlobClient CreateCloudBlobClient()
+        {
+            return CreateCloudStorageAccount().CreateCloudBlobClient();
+        }
+    }
+}
---
+++ src/ToyStorage/DocumentCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace ToyStorage
+{
+    public class DocumentCollection : IDocumentCollection
+    {
+        private readonly CloudBlobContainer _container;
+        private readonly JsonSerializer _serializer;
+
+        public DocumentCollection(CloudBlobContainer container, JsonSerializer serializer)
+        {
+            _container = container ?? throw new ArgumentNullException(nameof(container));
+            _serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
+        }
+
+        public async Task<TEntity> GetAsync<TEntity>(string id)
+        {
+            if (id == null) throw new ArgumentNullException(nameof(id));
+
+            var blob = GetBlob(id);
+
+            using (var reader = new JsonTextReader(new StreamReader(await blob.OpenReadAsync())))
+            {
+                return _serializer.Deserialize<TEntity>(reader);
+            }
+        }
+
+        public async Task StoreAsync(object entity, string id)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            if (id == null) throw new ArgumentNullException(nameof(id));
+
+            var blob = GetBlob(id);
+
+            using (var writer = new JsonTextWriter(new StreamWriter(await blob.OpenWriteAsync())))
+            {
+                _serializer.Serialize(writer, entity);
+            }
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            if (id == null) throw new ArgumentNullException(nameof(id));
+
+            var blob = GetBlob(id);
+
+            return blob.DeleteAsync();
+        }
+
+        private CloudBlockBlob GetBlob(string id)
+        {
+            return _container.GetBlockBlobReference(id);
+        }
+    }
+}
