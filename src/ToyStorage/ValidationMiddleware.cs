96ed8321ec0880414b25b332f261f6c0c42f62de
Kevin Herr <kherr9@gmail.com>
2017-10-05 16:39:47 -0500

    stub validation middleware

---
+++ src/ToyStorage/ValidationMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace ToyStorage
+{
+    public class ValidationMiddleware : IMiddleware
+    {
+        public Task Invoke(RequestContext context, RequestDelegate next)
+        {
+            var entity = context.Entity;
+            if (entity != null)
+            {
+                Validator.ValidateObject(entity, new ValidationContext(entity, null, null));
+            }
+
+            return next(context);
+        }
+    }
+}
