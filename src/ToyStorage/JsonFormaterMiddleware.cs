4880b0fc63f6f50c635f72db21e665891a091890
Kevin Herr <kherr@gcmlp.com>
2017-10-10 16:40:39 -0500

    refactor content type in json formatter

--- src/ToyStorage/JsonFormaterMiddleware.cs
+++ src/ToyStorage/JsonFormaterMiddleware.cs
@@ -5,21 +5,23 @@ using Newtonsoft.Json;
 namespace ToyStorage
 {
     /// <summary>
-    /// Middleware component that formats object to binary to be used downstream for persistence.
+    /// Middleware component that formats object to binary (writes) and binary to object (reads).
     /// </summary>
     public class JsonFormaterMiddleware : IMiddleware
     {
+        private const string JsonContentType = "application/json";
+
         public async Task Invoke(RequestContext context, RequestDelegate next)
         {
             if (context.IsWrite())
             {
                 context.Content = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(context.Entity));
-                context.CloudBlockBlob.Properties.ContentType = "application/json";
+                context.CloudBlockBlob.Properties.ContentType = JsonContentType;
             }
 
             await next(context);
 
-            if (context.IsRead())
+            if (context.IsRead() && context.CloudBlockBlob.Properties.ContentType == JsonContentType)
             {
                 context.Entity = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(context.Content), context.EntityType);
             }
