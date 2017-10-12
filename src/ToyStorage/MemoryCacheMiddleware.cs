93801d4e09046c9a1b7b1f27cf14f93dc78892d8
Kevin Herr <kherr9@gmail.com>
2017-10-12 17:32:53 -0500

    add cache references

---
+++ src/ToyStorage/MemoryCacheMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+#if NET45
+using System.Runtime.Caching;
+#else
+using Microsoft.Extensions.Caching.Memory;
+#endif
+
+namespace ToyStorage
+{
+    public class MemoryCacheMiddleware : IMiddleware
+    {
+#if NET45
+        private readonly ObjectCache _cache;
+#else
+        private readonly IMemoryCache _cache;
+#endif
+        public Task Invoke(RequestContext context, RequestDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
