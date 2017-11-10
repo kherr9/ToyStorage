22ca29fbaae429d3a31ceddd767c2123ae256397
Kevin Herr <kherr@gcmlp.com>
2017-11-10 14:27:00 -0600

    comment

--- src/ToyStorage/JsonFormaterMiddlewareExtensions.cs
+++ src/ToyStorage/JsonFormaterMiddlewareExtensions.cs
@@ -4,6 +4,12 @@ namespace ToyStorage
 {
     public static class JsonFormaterMiddlewareExtensions
     {
+        /// <summary>
+        /// Use json formatter
+        /// </summary>
+        /// <param name="pipeline"></param>
+        /// <param name="setupAction"></param>
+        /// <returns></returns>
         public static MiddlewarePipeline UseJsonFormatter(this MiddlewarePipeline pipeline, Action<JsonOptions> setupAction = null)
         {
             var jsonOptions = new JsonOptions();
